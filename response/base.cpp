7cabc6f278abce8620593890bb52de0bde5de017
Don Goodman-Wilson <don@slack-corp.com>
2015-11-24 12:06:50 -0800

    Of course I left off a file.

---
+++ response/base.cpp
@@ -0,0 +1,45 @@
+//
+// Copyright © 2015 Slack Technologies, Inc. All rights reserved.
+//
+
+#include "slack/response/base.h"
+#include "config.h"
+#include <json/json.h>
+#include <string>
+
+namespace slack
+{
+namespace response
+{
+
+void base::parse(bool do_return)
+{
+    Json::Value result_ob;
+    Json::Reader reader;
+    bool parsedSuccess = reader.parse(raw_json, result_ob, false);
+    if (!parsedSuccess)
+    {
+        error = std::string{"json_parse_failure"};
+        return;
+    }
+
+    if (!result_ob["ok"].isBool())
+    {
+        error = std::string{"invalid_response"};
+        return;
+    }
+
+    auto ok = result_ob["ok"].asBool();
+
+    if (!ok)
+    {
+        error = result_ob["error"].asString();
+        if(do_return) return;
+    }
+    json_impl json{result_ob};
+    finish_parse(&json); //this is safe because the caller isn't hanging on to it.
+}
+
+
+} //namespace response
+} //namespace slack
