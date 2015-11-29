0cfcf4b923a3e1d1c86f3881c559df93a5af7b9f
Don Goodman-Wilson <don@slack-corp.com>
2015-11-29 14:16:34 -0800

    Simplify things again.

--- api/test.cpp
+++ api/test.cpp
@@ -9,10 +9,51 @@
 
 namespace slack
 {
-
 namespace api
 {
 
+namespace response
+{
+
+test::test(const std::string &raw_json)
+        : slack::base::response{raw_json}
+{
+    Json::Value result_ob = json_->json;
+
+    if (!result_ob["args"].isNull() && result_ob["args"].isObject())
+    {
+        args = std::map<std::string, std::string>{};
+
+        for (auto arg: result_ob["args"].getMemberNames())
+        {
+            args->emplace(arg, result_ob["args"][arg].asString());
+        }
+    }
+}
+
+} //namespace response
+
+namespace impl
+{
+
+response::test test::get_response()
+{
+    cpr::Parameters params; //no need for a token here
+    if (error_)
+    {
+        params.AddParameter({"error", *error_});
+    }
+    if (foo_)
+    {
+        params.AddParameter({"foo", *foo_});
+    }
+
+    auto result = cpr::Get(cpr::Url{slack_config::HOSTNAME + "api.test"}, params);
+
+    return handle_response(result.status_code, result.text);
+}
+
+} //namespace impl
 
 } //namespace api
 } //namespace slack
--- include/slack/api/test.h
+++ include/slack/api/test.h
@@ -5,14 +5,74 @@
 #pragma once
 
 #include <slack/types.h>
-#include <slack/api/parameter/test.h>
-#include <slack/api/response/test.h>
-#include <slack/api/impl/test.h>
+#include <slack/base/response.h>
+#include <slack/base/impl.h>
 
 namespace slack
 {
 namespace api
 {
+
+//parameters
+namespace parameter { namespace test {
+MAKE_STRING_LIKE(error);
+MAKE_STRING_LIKE(foo);
+} }
+
+//response
+namespace response
+{
+namespace error
+{
+namespace test
+{
+const auto UNKNOWN = std::string{"unknown"};
+const auto JSON_PARSE_FAILURE = std::string{"json_parse_failure"};
+const auto INVALID_RESPONSE = std::string{"invalid_response"};
+}
+}
+
+struct test :
+        public slack::base::response
+{
+    test(const std::string &raw_json);
+
+    std::experimental::optional<std::map<std::string, std::string>> args;
+};
+} //namespace response
+
+//impl
+namespace impl
+{
+
+
+
+class test :
+        public slack::base::impl<slack::api::response::test>
+{
+public:
+    //TODO can these be moved into the base class?
+    response::test get_response();
+
+    //TODO can we make a generic template?
+    void set_option(const parameter::test::error &error)
+    { error_ = error; }
+
+    void set_option(parameter::test::error &&error)
+    { error_ = std::move(error); }
+
+    void set_option(const parameter::test::foo &foo)
+    { foo_ = foo; }
+
+    void set_option(parameter::test::foo &&foo)
+    { foo_ = std::move(foo); }
+
+private:
+    std::experimental::optional<parameter::test::error> error_;
+    std::experimental::optional<parameter::test::foo> foo_;
+};
+
+} //namespace impl
 
 template<typename T>
 void set_option(impl::test &impl, T &&t)
