425c449c994abc1edee3f058e7f771cdd92476d0
Don Goodman-Wilson <don@slack-corp.com>
2015-12-02 10:11:08 -0800

    Uri decoding. Pretty important.

--- types.cpp
+++ types.cpp
@@ -4,6 +4,7 @@
 
 #include "slack/types.h"
 #include <json/json.h>
+#include "uricodec.h"
 
 namespace slack
 {
@@ -58,16 +59,16 @@ message::message(const Json::Value &parsed_json)
 
 command::command(const std::map<std::string, std::string> &params)
 {
-    if(params.count("token")) token = params.at("token");
-    if(params.count("team_id")) team_id = params.at("team_id");
-    if(params.count("team_domain")) team_domain = params.at("team_domain");
-    if(params.count("channel_id")) channel_id = params.at("channel_id");
-    if(params.count("channel_name")) channel_name = params.at("channel_name");
-    if(params.count("user_id")) user_id = params.at("user_id");
-    if(params.count("user_name")) user_name = params.at("user_name");
-    if(params.count("command")) command_name = params.at("command");
-    if(params.count("text")) text = params.at("text");
-    if(params.count("response_url")) response_url = params.at("response_url");
+    if(params.count("token")) token = UriDecode(params.at("token"));
+    if(params.count("team_id")) team_id = UriDecode(params.at("team_id"));
+    if(params.count("team_domain")) team_domain = UriDecode(params.at("team_domain"));
+    if(params.count("channel_id")) channel_id = UriDecode(params.at("channel_id"));
+    if(params.count("channel_name")) channel_name = UriDecode(params.at("channel_name"));
+    if(params.count("user_id")) user_id = UriDecode(params.at("user_id"));
+    if(params.count("user_name")) user_name = UriDecode(params.at("user_name"));
+    if(params.count("command")) command_name = UriDecode(params.at("command"));
+    if(params.count("text")) text = UriDecode(params.at("text"));
+    if(params.count("response_url")) response_url = UriDecode(params.at("response_url"));
 }
 
 }
---
+++ uricodec.h
@@ -0,0 +1,10 @@
+//
+// Copyright © 2015 Slack Technologies, Inc. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+
+std::string UriEncode(const std::string & sSrc);
+std::string UriDecode(const std::string & sSrc);
