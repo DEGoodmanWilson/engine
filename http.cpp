bfc877fae0b745a1f11a1890fe2a794aefc5c3de
Don Goodman-Wilson <don@slack-corp.com>
2015-12-06 11:35:22 -0800

    Use CPR by default now.

--- http.cpp
+++ http.cpp
@@ -3,14 +3,36 @@
 //
 
 #include "slack/http.h"
+#include <cpr.h>
 
 namespace slack
 {
 namespace http
 {
 
-std::function<response(std::string url, params)> get;
-std::function<response(std::string url, params)> post;
+std::function<response(std::string url, params)> get = [](std::string url, slack::http::params params) -> slack::http::response {
+    cpr::Parameters p;
+    for (auto &kv : params)
+    {
+        p.AddParameter({kv.first, kv.second});
+    }
+
+    auto response = cpr::Get(cpr::Url{url}, p);
+
+    return {static_cast<uint32_t>(response.status_code), response.text};
+};
+
+std::function<response(std::string url, params)> post = [](std::string url, slack::http::params params) -> slack::http::response {
+    cpr::Parameters p;
+    for (auto &kv : params)
+    {
+        p.AddParameter({kv.first, kv.second});
+    }
+
+    auto response = cpr::Post(cpr::Url{url}, p);
+
+    return {static_cast<uint32_t>(response.status_code), response.text};
+};
 
 } //namespace http
 } //namespace slack
