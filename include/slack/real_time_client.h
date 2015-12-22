582161accc9963fdf2b493c0d29e266c832cf590
Don Goodman-Wilson <don@slack-corp.com>
2015-12-22 07:51:53 -0800

    Some notes to myself

--- include/slack/real_time_client.h
+++ include/slack/real_time_client.h
@@ -11,7 +11,9 @@
 #include <memory>
 #include <functional>
 #include <thread>
-//TODO
+//TODO The way to do this is to factor out the websocket here, and use an abstract interface that
+// the user can optionally provide, just as we do with CPR
+
 #include <Simple-WebSocket-Server/client_wss.hpp>
 
 // We need to think very carefully about the threading model. Who owns the thread?
@@ -23,13 +25,25 @@ using WssClient = SimpleWeb::SocketClient<SimpleWeb::WSS>;
 class real_time_client : public event::event_handler
 {
 public:
-//    real_time_client(); //do this later
+    using on_open_callback_t = std::function<void()>;
+    using on_close_callback_t = std::function<void()>;
+    using on_error_callback_t = std::function<void()>;
 
     real_time_client(const std::string &url);
+
+    //TODO should we just make these fields public and assignable?
+    real_time_client(const std::string &url,
+                     on_open_callback_t on_open_callback,
+                     on_close_callback_t on_close_callback,
+                     on_error_callback_t on_error_callback
+    );
+
 
     void start();
 
     void stop();
+
+    //these can be used in case we don't want to use our own websocket implementation
 
     void handle_event_from_slack(const std::string& event_str);
 
@@ -49,6 +63,11 @@ protected:
 private:
     WssClient wss_client_;
     std::atomic<bool> is_connected_;
+
+    on_open_callback_t on_open_callback_;
+    on_close_callback_t on_close_callback_;
+    on_error_callback_t on_error_callback_;
+
     std::thread ping_thread_;
 
     void initialize_(void);
--- real_time_client.cpp
+++ real_time_client.cpp
@@ -17,21 +17,33 @@ void initialize_events(void); //declared in event.cpp
 
 void real_time_client::initialize_(void)
 {
-    event::initialize_events();
-}
-
-// This method will block until the connection is complete
-real_time_client::real_time_client(const std::string &url)
-        : wss_client_{std::string{url}.erase(0,6), false}, is_connected_{false}
-{
     wss_client_.onopen = std::bind(&real_time_client::on_open_, this);
     wss_client_.onclose = std::bind(&real_time_client::on_close_, this, std::placeholders::_1, std::placeholders::_2);
     wss_client_.onerror = std::bind(&real_time_client::on_error_, this, std::placeholders::_1);
     wss_client_.onmessage = std::bind(&real_time_client::on_message_, this, std::placeholders::_1);
 
+    event::initialize_events();
+}
+
+// This method will block until the connection is complete
+real_time_client::real_time_client(const std::string &url)
+        : wss_client_{std::string{url}.erase(0, 6), false}, is_connected_{false}
+{
     initialize_();
 }
 
+
+real_time_client::real_time_client(const std::string &url,
+                                   on_open_callback_t on_open_callback,
+                                   on_close_callback_t on_close_callback,
+                                   on_error_callback_t on_error_callback)
+        : wss_client_{std::string{url}.erase(0, 6), false}, is_connected_{false},
+          on_open_callback_{on_open_callback},
+          on_close_callback_{on_close_callback},
+          on_error_callback_{on_error_callback}
+{
+    initialize_();
+}
 
 void real_time_client::start()
 {
@@ -49,14 +61,19 @@ void real_time_client::on_open_()
 {
     is_connected_ = true; //it's atomic, so this is ok!
     // And start the ping thread
+
+    //and notifiy the user
+    on_open_callback_();
 }
 
 
-void real_time_client::on_close_(int status, const std::string& reason)
+void real_time_client::on_close_(int status, const std::string &reason)
 {
     is_connected_ = false;
     //TODO other things.
     // and stop the ping thread
+
+    on_close_callback_();
 }
 
 
@@ -64,6 +81,8 @@ void real_time_client::on_error_(const boost::system::error_code &error_code)
 {
     //TODO
     std::cout << error_code << std::endl;
+
+    on_error_callback_();
 }
 
 
