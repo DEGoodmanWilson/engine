e6b8536bcb15060f3d6ac054d122c8c951e36c57
Don Goodman-Wilson <don@slack-corp.com>
2016-07-07 11:01:26 -0700

    Make the test not crash.

--- tests/main.cpp
+++ tests/main.cpp
@@ -13,6 +13,12 @@ int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
 
+    if(!std::getenv("SLACK_TOKEN"))
+    {
+        std::cerr << "please set environment variable SLACK_TOKEN" << std::endl;
+        exit(1);
+    }
+
     env = dynamic_cast<::Environment*>(::testing::AddGlobalTestEnvironment(new ::Environment(std::getenv("SLACK_TOKEN"))));
 
     return RUN_ALL_TESTS();
